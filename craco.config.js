26f28b5173b60f5d28a1a5a40f35c165a57d4fd8
Katie Crain <kt.crain@gmail.com>
2021-03-06 02:09:24 -0500

    terser

---
+++ craco.config.js
@@ -0,0 +1,10 @@
+const path = require("path");
+const webpack = require("webpack");
+const TerserPlugin = require("terser-webpack-plugin");
+
+module.exports = {
+  optimization: {
+    minimize: true,
+    minimizer: [new TerserPlugin()],
+  },
+};
